92e55270a53bee45ab9d87635645481b102e98dc
rozap <chrisd1891@gmail.com>
2025-10-27 15:15:59 -0700

    support for PDF content in gemini

--- lib/chat_models/chat_google_ai.ex
+++ lib/chat_models/chat_google_ai.ex
@@ -294,6 +294,27 @@ defmodule LangChain.ChatModels.ChatGoogleAI do
     }
   end
 
+  def for_api(%ContentPart{type: :file, options: opts} = part) do
+    media = Keyword.get(opts || [], :media, nil)
+
+    mime_type =
+      if is_nil(media) do
+        message = "Received no media type for ContentPart"
+        Logger.error(message)
+        raise LangChainError, message
+      else
+        "#{media}"
+      end
+
+
+    %{
+      "inline_data" => %{
+        "mime_type" => mime_type,
+        "data" => part.content
+      }
+    }
+  end
+
   def for_api(%Message{content: content} = message) when is_binary(content) do
     %{
       "role" => map_role(message.role),
